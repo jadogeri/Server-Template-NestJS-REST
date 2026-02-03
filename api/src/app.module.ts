216eccd4cc3699d27dbb72c0b97e7e8a288bedcf
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-03 03:17:47 -0600

    Added role module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { SessionModule } from './modules/session/session.module';
 import { ProfileModule } from './modules/profile/profile.module';
+import { RoleModule } from './modules/role/role.module';
 
 @Module({
-  imports: [UserModule, AuthModule, SessionModule, ProfileModule],
+  imports: [UserModule, AuthModule, SessionModule, ProfileModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
