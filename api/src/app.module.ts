855eb50892f46d9e174bfb8727982be8a6de52e3
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-03 03:15:48 -0600

    Added session module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { SessionModule } from './modules/session/session.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
