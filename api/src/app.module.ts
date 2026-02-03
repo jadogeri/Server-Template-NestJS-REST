20efce76e551212110bd70d283e54a772902edb7
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-03 03:16:44 -0600

    Added profile module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { SessionModule } from './modules/session/session.module';
+import { ProfileModule } from './modules/profile/profile.module';
 
 @Module({
-  imports: [UserModule, AuthModule, SessionModule],
+  imports: [UserModule, AuthModule, SessionModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
