1b155677d90f9f16741df6dc78a523cc78097b72
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2026-02-04 23:38:28 -0600

    Added type orm config

---
+++ api/src/configs/type-orm.config.ts
@@ -0,0 +1,47 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { SeederOptions } from 'typeorm-extension';
+  import { DataSource } from 'typeorm';
+
+// import UserSeeder from '../resources/user/user.seeder';
+// import { UserFactory } from '../resources/user/user.factory';
+import dotenv from 'dotenv';
+import { User } from '../modules/user/entities/user.entity';
+
+
+dotenv.config();
+
+console.log("Loading TypeORM configuration...");
+console.log("Node Environment:", process.env.NODE_ENV || 'development');
+console.log("dirname in type-orm.config.ts:", __dirname);
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+const databaseFile = isProduction ? process.env.DATABASE || "prodDB.sqlite" : 'devDB.sqlite';
+console.log("Using database file:", databaseFile);
+
+
+
+export const dataSourceOptions: TypeOrmModuleOptions & SeederOptions = {
+  type: 'sqlite',
+  database: databaseFile,
+  // {ts,js} matches both source and compiled files
+    // 🔍 Check these paths carefully!
+
+  entities: [__dirname + '/../modules/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../core/database/migrations/*{.ts,.js}'],
+  subscribers: [__dirname + '/../core/database/subscribers/*{.ts,.js}'],
+  // seeds: [UserSeeder],
+  // factories: [UserFactory],
+  synchronize: !isProduction,
+  logging: isProduction === false ? ["query", "error", "schema"] : ["error"],
+  
+  // High-value for performance tuning: logs any query taking longer than 1 second
+  //maxQueryExecutionTime: 1000, 
+  
+  // Use 'formatted-console' for readable SQL in dev
+  logger: "advanced-console", };
+
+// export const AppDataSource = new DataSource(dataSourceOptions as any);
+
+
+export default dataSourceOptions;
+
