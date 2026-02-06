e768797c7238b03284a6435e36c8798ee33f9740
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-02-06 09:41:26 -0600

    Added auth generator util

--- api/src/common/utils/user-generator.util.ts
+++ api/src/common/utils/user-generator.util.ts
@@ -1,4 +1,4 @@
-import { User } from "src/modules/user/entities/user.entity";
+import { User } from "../../modules/user/entities/user.entity";
 
 export class UserGeneratorUtil {
   // Utility methods for generating users can be added here
