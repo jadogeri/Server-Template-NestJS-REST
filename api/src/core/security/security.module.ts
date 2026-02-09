cc90896eaf13c0f1126651e33f225c8da6d3b493
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2026-02-09 14:06:29 -0600

    Added CookieModule to SecurityModule

--- api/src/core/security/security.module.ts
+++ api/src/core/security/security.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { HashingModule } from './hashing/hashing.module';
 import { TokenModule } from './token/token.module';
 import { AccessControlModule } from './access-control/access-control.module';
+import { CookieModule } from './cookie/cookie.module';
 
 @Module({
-  imports: [HashingModule, TokenModule, AccessControlModule],
-  exports: [HashingModule, TokenModule, AccessControlModule], // Export services for use in other modules
+  imports: [HashingModule, TokenModule, AccessControlModule, CookieModule],
+  exports: [HashingModule, TokenModule, AccessControlModule, CookieModule], // Export services for use in other modules
   providers: [],
 })
 export class SecurityModule {}
